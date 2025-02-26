c2b4e54f090f63ee7d132cdb2a2d045be6253f16
root <root@romartin.42.fr>
2025-02-26 10:15:26 -0500

    c est fini ?

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -19,9 +19,9 @@ if [ ! -f /var/www/html/wp-config.php ]; then
  				--dbname=$MYSQL_DB \
  				--dbuser=$MYSQL_USER \
  				--dbpass=$MYSQL_PASSWORD \
- 				--dbhost=mariadb:3306 --path='/var/www/html'
+ 				--dbhost=mariadb --path='/var/www/html'
     echo "Wp core install"
-	wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_MAIL --skip-email --allow-root
+	wp core install --url=$DOMAIN_NAME/ --title="$WP_TITLE" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_MAIL --skip-email --allow-root
 	echo "Wp user create"
  	wp user create --allow-root ${WP_USER} ${WP_USER_MAIL} --user_pass=${WP_USER_PASSWORD};
 fi
